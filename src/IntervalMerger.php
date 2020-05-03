b0d8e0334f9fe359401eff6b4f38c9a4de13e969
Alexander Melnik <colddegree@yandex.ru>
2020-05-03 23:42:06 +1000

    слияние вещественных интервалов

--- src/IntervalMerger.php
+++ src/IntervalMerger.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App;
 
 use App\Entity\IntValue;
+use App\Entity\RealValue;
 use SplStack;
 
 class IntervalMerger
@@ -37,5 +38,66 @@ class IntervalMerger
         }
 
         return array_reverse(iterator_to_array($stack));
+    }
+
+    /**
+     * @param RealValue[] $intervals
+     *
+     * @return RealValue[]
+     */
+    public function mergeReal(array $intervals): array
+    {
+        if (count($intervals) <= 1) {
+            return $intervals;
+        }
+
+        $intervals = $this->sortRealIntervals($intervals);
+
+        $stack = new SplStack();
+        $stack->push($intervals[0]);
+
+        for ($i = 1, $iMax = count($intervals); $i < $iMax; ++$i) {
+            /** @var RealValue $topInterval */
+            $topInterval = $stack->top();
+            if ($topInterval->upper > $intervals[$i]->lower) {
+                $stack->pop();
+                $stack->push(new RealValue(
+                    $topInterval->lower,
+                    $topInterval->lowerIsInclusive,
+                    max($intervals[$i]->upper, $topInterval->upper),
+                    $intervals[$i]->upper > $topInterval->upper
+                        ? $intervals[$i]->upperIsInclusive
+                        : $topInterval->upperIsInclusive,
+                ));
+            } else {
+                $stack->push($intervals[$i]);
+            }
+        }
+
+        return array_reverse(iterator_to_array($stack));
+    }
+
+    /**
+     * @param RealValue[] $intervals
+     *
+     * @return RealValue[]
+     */
+    public function sortRealIntervals(array $intervals): array
+    {
+        $lowerBounds = array_map(static fn (RealValue $v) => $v->lower, $intervals);
+        $lowerBoundInclusiveFlags = array_map(static fn (RealValue $v) => (int)$v->lowerIsInclusive, $intervals);
+        $indices = array_keys($intervals);
+
+        array_multisort(
+            $lowerBounds, SORT_ASC, SORT_NUMERIC,
+            $lowerBoundInclusiveFlags, SORT_DESC, SORT_NUMERIC,
+            $indices, SORT_ASC, SORT_NUMERIC,
+        );
+
+        $result = [];
+        foreach ($indices as $index) {
+            $result[] = $intervals[$index];
+        }
+        return $result;
     }
 }
--- tests/IntervalMergerTest.php
+++ tests/IntervalMergerTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests;
 
 use App\Entity\IntValue;
+use App\Entity\RealValue;
 use App\IntervalMerger;
 use Generator;
 use PHPUnit\Framework\TestCase;
@@ -10,12 +11,13 @@ use PHPUnit\Framework\TestCase;
 class IntervalMergerTest extends TestCase
 {
     /**
+     * @test
      * @dataProvider intTestCases
      *
      * @param IntValue[] $intervals
      * @param IntValue[] $mergedIntervals
      */
-    public function testInt(array $intervals, array $mergedIntervals): void
+    public function int(array $intervals, array $mergedIntervals): void
     {
         $merger = new IntervalMerger();
 
@@ -52,8 +54,8 @@ class IntervalMergerTest extends TestCase
 
         yield 'второй интервал вложен в первый' => [
             [
-                new IntValue(2, 4),
                 new IntValue(1, 5),
+                new IntValue(2, 4),
             ],
             [new IntValue(1, 5)],
         ];
@@ -68,6 +70,84 @@ class IntervalMergerTest extends TestCase
                 new IntValue(1, 4),
                 new IntValue(5, 7),
             ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider realTestCases
+     *
+     * @param RealValue[] $intervals
+     * @param RealValue[] $mergedIntervals
+     */
+    public function real(array $intervals, array $mergedIntervals): void
+    {
+        $merger = new IntervalMerger();
+
+        $actual = $merger->mergeReal($intervals);
+
+        self::assertEquals($mergedIntervals, $actual);
+    }
+
+    public function realTestCases(): Generator
+    {
+        yield '2 интервала пересекаются' => [
+            [
+                new RealValue(1, false, 3, false),
+                new RealValue(-1, false,2, false),
+            ],
+            [new RealValue(-1, false, 3, false)],
+        ];
+
+        yield 'общее начало, но у первого нижняя граница не включительна' => [
+            [
+                new RealValue(1, false, 2, false),
+                new RealValue(1, true,5, false),
+            ],
+            [new RealValue(1, true, 5, false)],
+        ];
+
+        yield 'конец первого интервала касается начала второго интервала, но оба не включительны' => [
+            [
+                new RealValue(1, true, 2, false),
+                new RealValue(2, false,3, true),
+            ],
+            [
+                new RealValue(1, true, 2, false),
+                new RealValue(2, false,3, true),
+            ],
+        ];
+
+        yield 'первый невключительный интервал вложен во второй включительный' => [
+            [
+                new RealValue(2, false, 3, false),
+                new RealValue(1, true,5, true),
+            ],
+            [new RealValue(1, true, 5, true)],
+        ];
+
+        yield 'первый невключительный пересекается "справа" со вторым включительным интервалом' => [
+            [
+                new RealValue(4, false, 6, false),
+                new RealValue(1, true,5, true),
+            ],
+            [new RealValue(1, true, 6, false)],
+        ];
+
+        yield '2 интервала с одинаковыми невключительными нижними границами' => [
+            [
+                new RealValue(1, false, 5, false),
+                new RealValue(1, false,3, false),
+            ],
+            [new RealValue(1, false, 5, false)],
+        ];
+
+        yield '2 интервала с одинаковыми включительными нижними границами' => [
+            [
+                new RealValue(1, true, 5, false),
+                new RealValue(1, true,3, false),
+            ],
+            [new RealValue(1, true, 5, false)],
         ];
     }
 }
