70bc4fe7cdebe8947530a16bfd4011f56749aea6
Alexander Melnik <colddegree@yandex.ru>
2020-05-09 15:01:17 +1000

    признаки: переименовать FeatureReactController в FeatureController

--- src/Controller/FeatureController.php
+++ src/Controller/FeatureController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class FeatureReactController extends AbstractReactController
+class FeatureController extends AbstractReactController
 {
     private EntityManagerInterface $entityManager;
     private ObjectRepository $featureRepository;
