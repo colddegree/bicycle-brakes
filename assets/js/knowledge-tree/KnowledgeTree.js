c39980587bd55079554a69e15d63e453dfde9f27
Alexander Melnik <colddegree@yandex.ru>
2020-05-09 12:37:47 +1000

    дерево знаний: добавить поддерево с неисправностями, поправить поддерево с признаками

--- assets/js/knowledge-tree/KnowledgeTree.js
+++ assets/js/knowledge-tree/KnowledgeTree.js
@@ -7,6 +7,16 @@ import { CloseCircleTwoTone, CheckCircleTwoTone } from '@ant-design/icons';
 const Ok = () => <CheckCircleTwoTone twoToneColor="#5ace00" />;
 const Fail = () => <CloseCircleTwoTone twoToneColor="#ff5268" />;
 
+
+const featuresAreOk = features => {
+    return !featuresAreEmpty(features)
+        && features.reduce((acc, f) => acc = acc && featureIsOk(f), true);
+};
+
+const featuresAreEmpty = features => {
+    return features.length < 1;
+};
+
 const featureIsOk = feature => {
     return featureHasPossibleValues(feature)
         && featureHasNormalValues(feature)
@@ -25,35 +35,78 @@ const featureNormalValuesAreSubsetOfPossibleValues = feature => {
     return feature.normalValues.values.reduce((acc, v) => acc = acc && v.isSubsetOfPossibleValues, true);
 };
 
+
+const malfunctionsAreOk = malfunctions => {
+    return !malfunctionsAreEmpty(malfunctions)
+        && malfunctions.reduce((acc, m) => acc = acc && malfunctionIsOk(m), true);
+};
+
+const malfunctionsAreEmpty = malfunctions => {
+    return malfunctions.length < 1;
+};
+
+const malfunctionIsOk = malfunction => {
+    return malfunctionClinicalPictureHasFeatures(malfunction)
+        && malfunctionClinicalPictureIsOk(malfunction);
+};
+
+const malfunctionClinicalPictureHasFeatures = ({ clinicalPicture }) => {
+    return clinicalPicture.length > 0;
+};
+
+const malfunctionClinicalPictureIsOk = ({ clinicalPicture }) => {
+    return clinicalPicture
+        .reduce((acc, f) => acc = acc && malfunctionClinicalPictureFeatureIsOk(f), true);
+};
+
+const malfunctionClinicalPictureFeatureIsOk = feature => {
+    return !malfunctionFeatureValuesAreEmpty(feature) && malfunctionFeatureValuesAreOk(feature);
+};
+
+const malfunctionFeatureValuesAreEmpty = feature => {
+    const { values } = feature.malfunctionFeatureValues;
+    return values.length < 1;
+};
+
+const malfunctionFeatureValuesAreOk = feature => {
+    const { values } = feature.malfunctionFeatureValues;
+    return values.reduce((acc, v) => acc = acc && v.isSubsetOfPossibleValues && !v.isIntersectsWithNormalValues, true);
+};
+
+
 const KnowledgeTree = ({ features, malfunctions }) => {
     const treeData = [
         {
             title: 'Знания',
             key: 'root',
+            icon: featuresAreOk(features) && malfunctionsAreOk(malfunctions) ? <Ok /> : <Fail />,
             children: [
                 {
-                    title: 'Признаки',
+                    title: featuresAreEmpty(features)
+                        ? <>Признаки <i>[отсутствуют]</i></>
+                        : 'Признаки',
                     key: 'features',
-                    icon: !features.reduce((acc, f) => acc = acc && featureIsOk(f), true) ? <Fail /> : <Ok />,
+                    icon: featuresAreOk(features) ? <Ok /> : <Fail />,
                     children: features.map(f => ({
                         title: `${f.name} (#${f.id})`,
-                        key: f.id,
+                        key: `f${f.id}`,
                         icon: !featureIsOk(f) ? <Fail /> : <Ok />,
                         children: [
                             {
                                 title: !featureHasPossibleValues(f)
                                     ? <>Возможные значения <i>[значения отсуствуют]</i></>
                                     : 'Возможные значения',
-                                key: `pvs_${f.id}`,
+                                key: `f${f.id}_pv`,
                                 icon: !featureHasPossibleValues(f) ? <Fail /> : <Ok />,
                                 children: !featureHasPossibleValues(f)
                                         ? null
                                         : [
                                             {
                                                 title: f.possibleValues.summary,
-                                                key: `pvs_${f.id}_summary`,
+                                                key: `f${f.id}_pv_s`,
                                                 children: f.possibleValues.values.map(v => ({
-                                                    title: v,
+                                                    title: v.value,
+                                                    key: `f${f.id}_pv_s_v${v.id}`,
                                                 })),
                                             }
                                         ],
@@ -64,7 +117,7 @@ const KnowledgeTree = ({ features, malfunctions }) => {
                                     : !featureNormalValuesAreSubsetOfPossibleValues(f)
                                         ? <>Нормальные значения <i>[не являются подмножеством возможных значений]</i></>
                                         : 'Нормальные значения',
-                                key: `nvs_${f.id}`,
+                                key: `f${f.id}_nv`,
                                 icon: !featureHasNormalValues(f) || !featureNormalValuesAreSubsetOfPossibleValues(f)
                                     ? <Fail />
                                     : <Ok />,
@@ -73,10 +126,11 @@ const KnowledgeTree = ({ features, malfunctions }) => {
                                     : [
                                         {
                                             title: f.normalValues.summary,
-                                            key: `nvs_${f.id}_summary`,
+                                            key: `f${f.id}_nv_s`,
                                             icon: !featureNormalValuesAreSubsetOfPossibleValues(f) ? <Fail /> : <Ok />,
                                             children: f.normalValues.values.map(v => ({
                                                 title: v.value,
+                                                key: `f${f.id}_nv_s_v${v.id}`,
                                                 icon: !v.isSubsetOfPossibleValues ? <Fail/> : <Ok />
                                             })),
                                         },
@@ -85,7 +139,64 @@ const KnowledgeTree = ({ features, malfunctions }) => {
                         ],
                     })),
                 },
-                // TODO: malfunctions
+                {
+                    title: malfunctionsAreEmpty(malfunctions)
+                        ? <>Неисправности <i>[отсутствуют]</i></>
+                        : 'Неисправности',
+                    key: 'malfunctions',
+                    icon: malfunctionsAreOk(malfunctions) ? <Ok /> : <Fail />,
+                    children: malfunctions.map(m => ({
+                        title: `${m.name} (#${m.id})`,
+                        key: `m${m.id}`,
+                        icon: malfunctionIsOk(m) ? <Ok /> : <Fail />,
+                        children: [
+                            {
+                                title: !malfunctionClinicalPictureHasFeatures(m)
+                                    ? <>Клиническая картина <i>[признаки отсутствуют]</i></>
+                                    : 'Клиническая картина',
+                                key: `m${m.id}_cc`,
+                                icon: !malfunctionClinicalPictureHasFeatures(m) || !malfunctionClinicalPictureIsOk(m)
+                                    ? <Fail />
+                                    : <Ok />,
+                                children: m.clinicalPicture.map(f => ({
+                                    title: `${f.name} (#${f.id})`,
+                                    key: `m${m.id}_cc_f${f.id}`,
+                                    icon: !malfunctionClinicalPictureFeatureIsOk(f) ? <Fail /> : <Ok />,
+                                    children: [
+                                        {
+                                            title: malfunctionFeatureValuesAreEmpty(f)
+                                                ? <>Значения <i>[отсутствуют]</i></>
+                                                : 'Значения',
+                                            key: `m${m.id}_cc_f${f.id}_v`,
+                                            icon: malfunctionFeatureValuesAreEmpty(f)
+                                                || !malfunctionFeatureValuesAreOk(f) ? <Fail /> : <Ok />,
+                                            children: malfunctionFeatureValuesAreEmpty(f)
+                                                ? null
+                                                : [
+                                                    {
+                                                        title: f.malfunctionFeatureValues.summary,
+                                                        key: `m${m.id}_cc_f${f.id}_v_s`,
+                                                        icon: !malfunctionFeatureValuesAreOk(f) ? <Fail /> : <Ok />,
+                                                        children: f.malfunctionFeatureValues.values.map(v => ({
+                                                            title: !v.isSubsetOfPossibleValues
+                                                                ? <>{v.value} <i>[не является подмножеством возможных значений]</i></>
+                                                                : v.isIntersectsWithNormalValues
+                                                                    ? <>{v.value} <i>[пересекается с нормальными значениями]</i></>
+                                                                    : v.value,
+                                                            key: `m${m.id}_cc_f${f.id}_v_s_t${f.type}_v${v.id}`,
+                                                            icon: !v.isSubsetOfPossibleValues || v.isIntersectsWithNormalValues
+                                                                ? <Fail />
+                                                                : <Ok />,
+                                                        })),
+                                                    }
+                                                ],
+                                        },
+                                    ],
+                                })),
+                            },
+                        ],
+                    })),
+                },
             ],
         },
     ];
@@ -105,18 +216,36 @@ KnowledgeTree.propTypes = {
         type: PropTypes.number.isRequired,
         possibleValues: PropTypes.shape({
             summary: PropTypes.string.isRequired,
-            values: PropTypes.arrayOf(PropTypes.string).isRequired,
+            values: PropTypes.arrayOf(PropTypes.shape({
+                id: PropTypes.number.isRequired,
+                value: PropTypes.string.isRequired,
+            })).isRequired,
         }).isRequired,
         normalValues: PropTypes.shape({
             summary: PropTypes.string.isRequired,
             values: PropTypes.arrayOf(PropTypes.shape({
+                id: PropTypes.number.isRequired,
                 value: PropTypes.string.isRequired,
                 isSubsetOfPossibleValues: PropTypes.bool.isRequired,
             })).isRequired,
         }).isRequired,
     })).isRequired,
     malfunctions: PropTypes.arrayOf(PropTypes.shape({
-        // TODO
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        clinicalPicture: PropTypes.arrayOf(PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+            malfunctionFeatureValues: PropTypes.shape({
+                summary: PropTypes.string.isRequired,
+                values: PropTypes.arrayOf(PropTypes.shape({
+                    id: PropTypes.number.isRequired,
+                    value: PropTypes.string.isRequired,
+                    isSubsetOfPossibleValues: PropTypes.bool.isRequired,
+                    isIntersectsWithNormalValues: PropTypes.bool.isRequired,
+                })).isRequired,
+            }).isRequired,
+        })).isRequired,
     })).isRequired,
 };
 
--- src/KnowledgeTree/KnowledgeTreeCreator.php
+++ src/KnowledgeTree/KnowledgeTreeCreator.php
@@ -8,7 +8,10 @@ use App\Entity\Feature;
 use App\Entity\FeatureNormalValue;
 use App\Entity\FeaturePossibleValue;
 use App\Entity\IntValue;
+use App\Entity\Malfunction;
+use App\Entity\MalfunctionFeatureValueBind;
 use App\Entity\RealValue;
+use App\Entity\ScalarValue;
 use App\Mapper\IntIntervalsToStringMapper;
 use App\Mapper\RealIntervalsToStringMapper;
 use Doctrine\ORM\EntityManagerInterface;
@@ -17,23 +20,28 @@ use RuntimeException;
 
 class KnowledgeTreeCreator
 {
-    private EntityManagerInterface $entityManager;
     private ObjectRepository $featureRepository;
     private IntIntervalsToStringMapper $intIntervalsToStringMapper;
     private RealIntervalsToStringMapper $realIntervalsToStringMapper;
     private SubsetValidator $subsetValidator;
+    private ObjectRepository $malfunctionRepository;
+    private ObjectRepository $malfunctionFeatureValueBindRepository;
+    private IntersectValidator $intersectValidator;
 
     public function __construct(
         EntityManagerInterface $entityManager,
         IntIntervalsToStringMapper $intIntervalsToStringMapper,
         RealIntervalsToStringMapper $realIntervalsToStringMapper,
-        SubsetValidator $subsetValidator
+        SubsetValidator $subsetValidator,
+        IntersectValidator $intersectValidator
     ) {
-        $this->entityManager = $entityManager;
-        $this->featureRepository = $this->entityManager->getRepository(Feature::class);
+        $this->featureRepository = $entityManager->getRepository(Feature::class);
         $this->intIntervalsToStringMapper = $intIntervalsToStringMapper;
         $this->realIntervalsToStringMapper = $realIntervalsToStringMapper;
         $this->subsetValidator = $subsetValidator;
+        $this->malfunctionRepository = $entityManager->getRepository(Malfunction::class);
+        $this->malfunctionFeatureValueBindRepository = $entityManager->getRepository(MalfunctionFeatureValueBind::class);
+        $this->intersectValidator = $intersectValidator;
     }
 
     public function create(): array
@@ -76,6 +84,10 @@ class KnowledgeTreeCreator
     {
         switch ($feature->type) {
             case Feature::TYPE_SCALAR:
+                $ids = $feature->possibleValues
+                    ->map(static fn (FeaturePossibleValue $fpv) => $fpv->scalarValue->id)
+                    ->toArray();
+
                 $stringValues = $feature->possibleValues
                     ->map(static fn (FeaturePossibleValue $fpv) => $fpv->scalarValue->value)
                     ->toArray();
@@ -86,6 +98,10 @@ class KnowledgeTreeCreator
                 break;
 
             case Feature::TYPE_INT:
+                $ids = $feature->possibleValues
+                    ->map(static fn (FeaturePossibleValue $fpv) => $fpv->intValue->id)
+                    ->toArray();
+
                 $valueCollection = $feature->possibleValues->map(static fn (FeaturePossibleValue $fpv) => $fpv->intValue);
                 $summary = $this->intIntervalsToStringMapper->map($valueCollection->toArray());
                 $stringValues = $valueCollection
@@ -94,6 +110,10 @@ class KnowledgeTreeCreator
                 break;
 
             case Feature::TYPE_REAL:
+                $ids = $feature->possibleValues
+                    ->map(static fn (FeaturePossibleValue $fpv) => $fpv->realValue->id)
+                    ->toArray();
+
                 $valueCollection = $feature->possibleValues->map(static fn (FeaturePossibleValue $fpv) => $fpv->realValue);
                 $summary = $this->realIntervalsToStringMapper->map($valueCollection->toArray());
                 $stringValues = $valueCollection
@@ -105,13 +125,23 @@ class KnowledgeTreeCreator
                 throw new RuntimeException(sprintf('Unsupported type "%s"', $feature->type));
         }
 
-        return [$summary, $stringValues];
+        return [
+            $summary,
+            array_map(static fn (int $id, string $value) => [
+                'id' => $id,
+                'value' => $value,
+            ], $ids, $stringValues),
+        ];
     }
 
     private function fetchNormalValuesSubtree(Feature $feature): array
     {
         switch ($feature->type) {
             case Feature::TYPE_SCALAR:
+                $ids = $feature->normalValues
+                    ->map(static fn (FeatureNormalValue $fnv) => $fnv->scalarValue->id)
+                    ->toArray();
+
                 $stringValueCollection = $feature->normalValues->map(
                     static fn (FeatureNormalValue $fnv) => $fnv->scalarValue->value,
                 );
@@ -123,16 +153,19 @@ class KnowledgeTreeCreator
                 );
                 $stringValues = $stringValueCollection->toArray();
 
-                $possibleValueStrings = $feature->possibleValues->map(
-                    static fn (FeaturePossibleValue $fpv) => $fpv->scalarValue->value,
-                )->toArray();
-                $isSubsetOfPossibleValuesFlags = $stringValueCollection->map(
-                    static fn (string $s) => in_array($s, $possibleValueStrings, true),
-                )->toArray();
+                $possibleValueStrings = $feature->possibleValues
+                    ->map(static fn (FeaturePossibleValue $fpv) => $fpv->scalarValue->value)
+                    ->toArray();
+                $isSubsetOfPossibleValuesFlags = $stringValueCollection
+                    ->map(static fn (string $s) => in_array($s, $possibleValueStrings, true))
+                    ->toArray();
                 break;
 
-
             case Feature::TYPE_INT:
+                $ids = $feature->normalValues
+                    ->map(static fn (FeatureNormalValue $fnv) => $fnv->intValue->id)
+                    ->toArray();
+
                 $valueCollection = $feature->normalValues->map(static fn (FeatureNormalValue $fnv) => $fnv->intValue);
                 $summary = $this->intIntervalsToStringMapper->map($valueCollection->toArray());
                 $stringValues = $valueCollection
@@ -148,6 +181,10 @@ class KnowledgeTreeCreator
                 break;
 
             case Feature::TYPE_REAL:
+                $ids = $feature->normalValues
+                    ->map(static fn (FeatureNormalValue $fnv) => $fnv->realValue->id)
+                    ->toArray();
+
                 $valueCollection = $feature->normalValues->map(static fn (FeatureNormalValue $fnv) => $fnv->realValue);
                 $summary = $this->realIntervalsToStringMapper->map($valueCollection->toArray());
                 $stringValues = $valueCollection
@@ -168,15 +205,134 @@ class KnowledgeTreeCreator
 
         return [
             $summary,
-            array_map(static fn (string $value, bool $flag) => [
+            array_map(static fn (int $id, string $value, bool $flag) => [
+                'id' => $id,
                 'value' => $value,
                 'isSubsetOfPossibleValues' => $flag,
-            ], $stringValues, $isSubsetOfPossibleValuesFlags),
+            ], $ids, $stringValues, $isSubsetOfPossibleValuesFlags),
         ];
     }
 
     private function fetchMalfunctionsSubtree(): array
     {
-        return []; // TODO
+        $tree = [];
+
+        /** @var Malfunction[] $malfunctions */
+        $malfunctions = $this->malfunctionRepository->findAll();
+
+        foreach ($malfunctions as $m) {
+            $tree[] = [
+                'id' => $m->id,
+                'name' => $m->name,
+                'clinicalPicture' => $m->features->map(fn (Feature $f) => [
+                    'id' => $f->id,
+                    'name' => $f->name,
+                    'malfunctionFeatureValues' => $this->fetchMalfunctionFeatureValuesSubtree(
+                        current($this->malfunctionFeatureValueBindRepository->findBy([
+                            'malfunction' => $m,
+                            'feature' => $f,
+                        ]))),
+                ])->toArray(),
+            ];
+        }
+
+        return $tree;
+    }
+
+    private function fetchMalfunctionFeatureValuesSubtree(MalfunctionFeatureValueBind $bind): array
+    {
+        switch ($bind->feature->type) {
+            case Feature::TYPE_SCALAR:
+                $ids = $bind->scalarValues->map(static fn (ScalarValue $v) => $v->id)->toArray();
+
+                $stringValueCollection = $bind->scalarValues->map(static fn (ScalarValue $v) => $v->value);
+                $summary = sprintf(
+                    '{%s}',
+                    implode(', ', $stringValueCollection->map(
+                        static fn (string $s) => sprintf('"%s"', $s),
+                    )->toArray()),
+                );
+                $stringValues = $stringValueCollection->toArray();
+
+                $possibleValueStrings = $bind->feature->possibleValues
+                    ->map(static fn (FeaturePossibleValue $fpv) => $fpv->scalarValue->value)
+                    ->toArray();
+                $isSubsetOfPossibleValuesFlags = $stringValueCollection
+                    ->map(static fn (string $s) => in_array($s, $possibleValueStrings, true))
+                    ->toArray();
+
+                $normalValueStrings = $bind->feature->normalValues
+                    ->map(static fn (FeatureNormalValue $fnv) => $fnv->scalarValue->value)
+                    ->toArray();
+                $isIntersectsWithNormalValuesFlags = $stringValueCollection
+                    ->map(static fn (string $s) => in_array($s, $normalValueStrings, true))
+                    ->toArray();
+                break;
+
+            case Feature::TYPE_INT:
+                $ids = $bind->intValues->map(static fn (IntValue $v) => $v->id)->toArray();
+
+                $summary = $this->intIntervalsToStringMapper->map($bind->intValues->toArray());
+                $stringValues = $bind->intValues
+                    ->map(fn (IntValue $v) => $this->intIntervalsToStringMapper->map([$v]))
+                    ->toArray();
+
+                $possibleValues = $bind->feature->possibleValues
+                    ->map(static fn (FeaturePossibleValue $fpv) => $fpv->intValue)
+                    ->toArray();
+                $isSubsetOfPossibleValuesFlags = $bind->intValues
+                    ->map(fn (IntValue $v) => $this->subsetValidator->checkAsAreSubsetOfBsInt([$v], $possibleValues))
+                    ->toArray();
+
+                $normalValues = $bind->feature->normalValues
+                    ->map(static fn (FeatureNormalValue $fnv) => $fnv->intValue)
+                    ->toArray();
+                $isIntersectsWithNormalValuesFlags = $bind->intValues
+                    ->map(fn (IntValue $v) => $this->intersectValidator->checkAsIntersectsWithBsInt([$v], $normalValues))
+                    ->toArray();
+                break;
+
+            case Feature::TYPE_REAL:
+                $ids = $bind->realValues->map(static fn (RealValue $v) => $v->id)->toArray();
+
+                $summary = $this->realIntervalsToStringMapper->map($bind->realValues->toArray());
+                $stringValues = $bind->realValues
+                    ->map(fn (RealValue $v) => $this->realIntervalsToStringMapper->map([$v]))
+                    ->toArray();
+
+                $possibleValues = $bind->feature->possibleValues
+                    ->map(static fn (FeaturePossibleValue $fpv) => $fpv->realValue)
+                    ->toArray();
+                $isSubsetOfPossibleValuesFlags = $bind->realValues
+                    ->map(fn (RealValue $v) => $this->subsetValidator->checkAsAreSubsetOfBsReal([$v], $possibleValues))
+                    ->toArray();
+
+                $normalValues = $bind->feature->normalValues
+                    ->map(static fn (FeatureNormalValue $fnv) => $fnv->realValue)
+                    ->toArray();
+                $isIntersectsWithNormalValuesFlags = $bind->realValues
+                    ->map(fn (RealValue $v) => $this->intersectValidator->checkAsIntersectsWithBsReal([$v], $normalValues))
+                    ->toArray();
+                break;
+
+            default:
+                throw new RuntimeException(sprintf('Unsupported type "%s"', $bind->feature->type));
+        }
+
+        return [
+            'summary' => $summary,
+            'values' => array_map(
+                static fn (int $id, string $value, bool $subsetFlag, bool $intersectFlag) => [
+                    'id' => $id,
+                    'value' => $value,
+                    'isSubsetOfPossibleValues' => $subsetFlag,
+                    'isIntersectsWithNormalValues' => $intersectFlag,
+                ],
+                $ids,
+                $stringValues,
+                $isSubsetOfPossibleValuesFlags,
+                $isIntersectsWithNormalValuesFlags,
+            ),
+        ];
     }
 }
